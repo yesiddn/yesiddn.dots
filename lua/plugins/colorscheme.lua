2d5d8bf65fc08ea558067de46a46bf9c3b07ab61
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-09 23:09:56 -0500

    Refactor color scheme configuration to include Kanagawa theme and remove unused plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,112 +1,99 @@
 return {
+  -- {
+  --   {
+  --     "xiyaowong/transparent.nvim",
+  --     config = function()
+  --       require("transparent").setup({
+  --         enable = false, -- boolean: enable transparent
+  --         extra_groups = { -- table/string: additional groups that should be cleared
+  --           "Normal",
+  --           "NormalNC",
+  --           "Comment",
+  --           "Constant",
+  --           "Special",
+  --           "Identifier",
+  --           "Statement",
+  --           "PreProc",
+  --           "Type",
+  --           "Underlined",
+  --           "Todo",
+  --           "String",
+  --           "Function",
+  --           "Conditional",
+  --           "Repeat",
+  --           "Operator",
+  --           "Structure",
+  --           "LineNr",
+  --           "NonText",
+  --           "SignColumn",
+  --           "CursorLineNr",
+  --           "EndOfBuffer",
+  --         },
+  --         exclude = {}, -- table: groups you don't want to clear
+  --       })
+  --       vim.cmd("TransparentEnable") -- execute the command to enable transparency
+  --     end,
+  --   },
+  --   { "rktjmp/lush.nvim" },
+  --   {
+  --     "anAcc22/sakura.nvim",
+  --     -- config = function()
+  --     --   vim.opt.background = "dark"
+  --     --   vim.cmd("colorscheme sakura")
+  --     -- end,
+  --   }
+  -- },
+  -- {
+  --   "dgox16/oldworld.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     variant = "oled",
+  --   },
+  -- },
+  -- {
+  --   "miikanissi/modus-themes.nvim",
+  --   name = "modus",
+  --   priority = 1000,
+  -- },
   {
-    {
-      "xiyaowong/transparent.nvim",
-      config = function()
-        require("transparent").setup({
-          enable = false, -- boolean: enable transparent
-          extra_groups = { -- table/string: additional groups that should be cleared
-            "Normal",
-            "NormalNC",
-            "Comment",
-            "Constant",
-            "Special",
-            "Identifier",
-            "Statement",
-            "PreProc",
-            "Type",
-            "Underlined",
-            "Todo",
-            "String",
-            "Function",
-            "Conditional",
-            "Repeat",
-            "Operator",
-            "Structure",
-            "LineNr",
-            "NonText",
-            "SignColumn",
-            "CursorLineNr",
-            "EndOfBuffer",
-          },
-          exclude = {}, -- table: groups you don't want to clear
-        })
-        vim.cmd("TransparentEnable") -- execute the command to enable transparency
-      end,
-    },
-    { "rktjmp/lush.nvim" },
-    {
-      "anAcc22/sakura.nvim",
-      config = function()
-        vim.opt.background = "dark"
-        vim.cmd("colorscheme sakura")
-      end,
-    }
-  },
-  {
-    "dgox16/oldworld.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      variant = "oled",
-    },
-  },
-  -- Plugin for the Kanagawa color scheme
-  {
-    "miikanissi/modus-themes.nvim",
-    name = "modus",
-    priority = 1000,
-  },
-  {
+    -- Plugin for the Kanagawa color scheme
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
-    priority = 1000,
+    priority = 1000, -- High priority to ensure it loads early
     opts = {
-      transparent = true,
-      theme = "dragon",
+      transparent = true, -- Enable transparent background
+      theme = "dragon", -- Set the theme variant to 'dragon'
       overrides = function(colors)
         local theme = colors.theme
         return {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-
-          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-
-          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-          PmenuSbar = { bg = theme.ui.bg_m1 },
-          PmenuThumb = { bg = theme.ui.bg_p2 },
+          NormalFloat = { bg = "none" }, -- Transparent background for floating windows
+          FloatBorder = { bg = "none" }, -- Transparent background for floating window borders
+          FloatTitle = { bg = "none" }, -- Transparent background for floating window titles
+  
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 }, -- Custom colors for dark mode
+  
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim }, -- Custom colors for Lazy plugin
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim }, -- Custom colors for Mason plugin
+  
+          FzfLuaNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 }, -- Custom colors for FzfLua normal
+          FzfLuaBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 }, -- Custom colors for FzfLua border
+          FzfLuaTitle = { fg = theme.ui.special, bold = true }, -- Custom colors for FzfLua title
+  
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- Custom colors for popup menu
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 }, -- Custom colors for selected item in popup menu
+          PmenuSbar = { bg = theme.ui.bg_m1 }, -- Custom colors for popup menu scrollbar
+          PmenuThumb = { bg = theme.ui.bg_p2 }, -- Custom colors for popup menu thumb
         }
       end,
     },
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa-dragon'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
   },
   {
     -- LazyVim configuration
     "LazyVim/LazyVim",
     opts = {
       -- Set the default color scheme
-      colorscheme = "sakura",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
